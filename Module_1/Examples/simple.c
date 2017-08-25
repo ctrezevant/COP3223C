748995862b298bdf9f7792e092c59757b63ed351
Charlton Trezevant <ct@ctis.me>
2017-08-25 11:45:19 -0400

    Add module 1 Example 1

---
+++ Module_1/Examples/simple.c
@@ -0,0 +1,14 @@
+/*Sarah Angell
+This program demonstrates a simple program in C
+*/
+
+//Pre-Processor Directives
+#include <stdio.h>
+
+//Main Function
+int main() {
+
+    printf("Welcome to C Programming!\n\n");
+
+    return 0;
+}
