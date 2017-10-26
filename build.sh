6cdc91b0c659396801d67e4afde2721edb7fbcc3
Charlton Trezevant <ct@ctis.me>
2017-10-25 23:54:11 -0400

    Significantly improved build.sh

--- build.sh
+++ build.sh
@@ -14,28 +14,38 @@ cd $SUBDIR
 
 MAKEFILE=`cat *akefile`
 
-echo "---------------------------------------------------------"
+echo -e "\n/////////////////////////////////////////////////////////\n"
 echo -e "\t ** NOW BUILDING: $SUBDIR **"
 
-  # Run make clean, if rule exists
-  if [ "$(echo "$MAKEFILE" | awk '$0 ~ /^all[\t ]*:/ {print}')" != "" ]
-  then
-    make
-  fi
-
+  TESTED=0
   # Run make test, if rule exists
   if [ "$(echo "$MAKEFILE" | awk '$0 ~ /^test[\t ]*:/ {print}')" != "" ]
   then
+    echo -e "\n>>> Running: make test"
     make test
+    TESTED=1
+    echo -e ">>> FINISHED: make test"
+  fi
+
+  # run make all, but only if the test rule isn't defined.
+  # By default make test will build the project and run tests, as well, so
+  # we don't need to build the project twice (once for all, again for tests)
+  if [ "$(echo "$MAKEFILE" | awk '$0 ~ /^all[\t ]*:/ {print}')" != "" ] && [ $TESTED == 0 ]
+  then
+    echo -e "\n>>> Running: make all"
+    make
+    echo -e ">>> FINISHED: make all"
   fi
 
   # Run make clean, if rule exists
   if [ "$(echo "$MAKEFILE" | awk '$0 ~ /^clean[\t ]*:/ {print}')" != "" ]
   then
+    echo -e "\n>>> Running: make clean"
     make clean
+    echo -e ">>> FINISHED: make clean"
   fi
 
-echo "---------------------------------------------------------"
+echo -e "\n/////////////////////////////////////////////////////////\n"
 
 cd ..
 
