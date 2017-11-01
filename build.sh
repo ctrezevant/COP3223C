5b6362ac6b9052b5f955cbe1ee1a666ab9188ade
Charlton Trezevant <ct@ctis.me>
2017-11-01 16:08:27 -0400

    Improve build.sh, now determines what to build automatically by checking commits

--- build.sh
+++ build.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
-# Travis Automatic Build Script
+# Travis CI Build Script - COP3223C
+# (c) 2017 Charlton Trezevant
+# MIT License
+# https://github.com/ctrezevant/COP3223C
 
-BLACKLIST="./Template"
-# Set to 1 to enable static code analysis (via scan-build)
+# Set to 1 to enable static code analysis before building (via scan-build)
 RUN_LINTER=1
 
-# Find subdirectories with makefiles (which we can then build and test), excluding the blacklist
-BUILDABLE_DIRS=`find . -name '*akefile' | grep -v $BLACKLIST | awk '{gsub(/[^\/]*$/,"");print}'`
+# List of subdirectories to blacklist (in our case, never build the Template project)
+BLACKLIST="./Template"
+
+# How many commits back should we check for changes to files?
+COMMIT_RANGE_LBOUND=10
+
+# Find subdirectories with new changes and makefiles (which we can then build and test), excluding the blacklist
+# CHANGED_DIRS will collect the list of new directories that contain new changes (in the past $COMMIT_RANGE_LBOUND commits) to .c source files
+# This will ensure that Travis attempts to build only the latest changes, which is what we want
+CHANGED_DIRS=`git diff --name-only HEAD~$COMMIT_RANGE_LBOUND..HEAD '*.c' | cut -d '/' -f1 | sed -e 's/^/.\//'`
+
+# BUILDABLE_DIRS will contain the list of directories with new/changed files that *also* have makefiles (and can therefore be built)
+BUILDABLE_DIRS=`find $CHANGED_DIRS -name '*akefile' | grep -v $BLACKLIST | awk '{gsub(/[^\/]*$/,"");print}'`
 
 for SUBDIR in $BUILDABLE_DIRS;
 do
@@ -66,5 +79,6 @@ done
 
 unset BLACKLIST
 unset RUN_LINTER
+unset CHANGED_DIRS
 unset BUILDABLE_DIRS
 unset MAKEFILE
